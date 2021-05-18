627695982b9a702876a96e457afbb133d842d392
dani-sfl <danita.sf@gmail.com>
2021-05-17 20:41:42 -0500

    TOP DOWN MOVEMENT Ajolote
    Hacer que el ajolote se mueva a la izquierda/derecha y arriba/abajo con las teclas de flechas y WASD, incluidas sus animaciones.

---
+++ Assets/movimientopersonaje.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimientopersonaje : MonoBehaviour
+{
+
+    public float velocidad = 5f;
+    public Rigidbody2D rb;
+    public Animator animator;
+
+    Vector2 movimiento;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Input
+        movimiento.x = Input.GetAxisRaw("Horizontal");
+        movimiento.y = Input.GetAxisRaw("Vertical");
+        animator.SetFloat("Horizontal", movimiento.x);
+        animator.SetFloat("Vertical", movimiento.y);
+        animator.SetFloat("Velocidad", movimiento.sqrMagnitude);
+    }
+    void FixedUpdate()
+    {
+        // Movimiento
+        rb.MovePosition(rb.position + movimiento*velocidad * Time.fixedDeltaTime);
+
+    }
+}
