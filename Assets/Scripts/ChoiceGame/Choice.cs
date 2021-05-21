fa27e0025a19f317e3fefacf4a04263e09fb3f23
Archana <A01335895@itesm.mx>
2021-05-20 23:06:13 -0500

    correction choices/ missing points counter

--- Assets/Scripts/ChoiceGame/Choice.cs
+++ Assets/Scripts/ChoiceGame/Choice.cs
@@ -15,42 +15,44 @@ public class Choice : MonoBehaviour
     public GameObject Choice05;
     public GameObject Choice06;
 
-    private int counterPoints= 0;
+    private int counterQuestions = 0;
+    //falta asignar los puntos y lo de ganar y perder
+    private int counterPoints;
 
     public void ChoiceOption1(){
-        counterPoints += 1;
-        if (counterPoints == 1){
+        counterQuestions  += 1;
+        if (counterQuestions  == 1){
             TextBox.GetComponent<Text>().text="A";    
         }
-        else if (counterPoints == 2){
+        else if (counterQuestions  == 2){
             TextBox.GetComponent<Text>().text="A2"; 
         }
-        else if (counterPoints == 3){
+        else if (counterQuestions  == 3){
             TextBox.GetComponent<Text>().text="A3"; 
         }
 
     }
 
     public void ChoiceOption2(){
-        counterPoints += 1;
-        if (counterPoints == 1){
+        counterQuestions  += 1;
+        if (counterQuestions  == 1){
             TextBox.GetComponent<Text>().text="B";    
         }
-        else if (counterPoints == 2){
+        else if (counterQuestions  == 2){
             TextBox.GetComponent<Text>().text="B2"; 
         }
-        else if (counterPoints == 3){
+        else if (counterQuestions  == 3){
             TextBox.GetComponent<Text>().text="B3"; 
         }
 
     }
 
     public void Prueba(){
-         counterPoints += 1;
-            if (counterPoints == 1){
+         counterQuestions  += 1;
+            if (counterQuestions  == 1){
             TextBox.GetComponent<Text>().text="Caca";    
         }
-        else if (counterPoints == 2){
+        else if (counterQuestions  == 2){
             TextBox.GetComponent<Text>().text="Caca1";    
         }
     }
@@ -64,19 +66,19 @@ public class Choice : MonoBehaviour
         Choice04.SetActive(false);
         Choice05.SetActive(false);
         Choice06.SetActive(false);
-        if (counterPoints == 1){
+        if (counterQuestions  == 1){
             Choice01.SetActive(false);
             Choice02.SetActive(false);
             Choice03.SetActive(true);
             Choice04.SetActive(true);
         }
-        else if (counterPoints == 2){
+        else if (counterQuestions  == 2){
             Choice03.SetActive(false);
             Choice04.SetActive(false);
             Choice05.SetActive(true);
             Choice06.SetActive(true);
         }
-        else if (counterPoints == 3){
+        else if (counterQuestions  == 3){
             Choice05.SetActive(false);
             Choice06.SetActive(false);
         }
