9c8f1dd4d36d1b3a0ceedf889580e4cafd405849
Archana <A01335895@itesm.mx>
2021-05-21 18:43:40 -0500

    scene 3.2 clouds dynamic, timer

---
+++ Assets/Scripts/Scene3.2/Spawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public int Seconds;
+    public GameObject TheGameObjectToSpawn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Spawn",0,Seconds);
+    }
+
+
+    void Spawn(){
+        Instantiate(TheGameObjectToSpawn, gameObject.transform.position, Quaternion.identity);
+    }
+}
