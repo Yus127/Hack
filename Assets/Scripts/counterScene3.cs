95f33580b726263b3300ee0d102ace9724212aac
Yusdivia Molina <yus.molina@gmail.com>
2021-05-22 20:58:49 -0500

    .

--- Assets/Scripts/counterScene3.cs
+++ Assets/Scripts/counterScene3.cs
@@ -3,18 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class QuitGame : MonoBehaviour
+public class counterScene3 : MonoBehaviour
 {
+public static int count=0;
     // Start is called before the first frame update
-    public void BtnNew()
+    void Start()
     {
-    counterScene3.count= counterScene3.count+1;
-        SceneManager.LoadScene("fiesta");
+        
     }
 
     // Update is called once per frame
     void Update()
     {
+    if(count==2){
+        print("ya es todo ");
+        SceneManager.LoadScene("final");
+        //Ended.gameObject.SetActive(true); 
+        //Destroy(mommy);
+        }
         
     }
 }
