0dd4756312bfa2f6df0682985ef806a7c39df192
gisgiselle <brengimolina@gmail.com>
2021-05-22 21:10:51 -0500

    Scene 1 Completely Finished
    Ya cambia de escena y se amplió la casa

--- Assets/Scripts/casita/GManager.cs
+++ Assets/Scripts/casita/GManager.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class GManager : MonoBehaviour
 {
 public static int count=0;
 public GameObject Ended;
 public GameObject mommy;
+
 
     // Start is called before the first frame update
     void Start()
@@ -21,7 +23,8 @@ public GameObject mommy;
         print("ya es todo ");
         Ended.gameObject.SetActive(true); 
         Destroy(mommy);
-        }
+        
     }
-    
+    }
+ 
 }
