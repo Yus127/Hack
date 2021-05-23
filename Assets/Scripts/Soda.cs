be5fdebe5addda9951661293811ed014e66e8082
Archana <A01335895@itesm.mx>
2021-05-23 13:16:43 -0500

    store linked to conversation
    Store scene linked to conversation scene from soda collision

---
+++ Assets/Scripts/Soda.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Soda : MonoBehaviour
+{
+  public Sprite Nuevo;
+public Sprite Viejo;
+public GameObject door;
+
+void OnCollisionEnter2D(Collision2D collision){
+        
+        if (collision.gameObject.tag == "Player" &&  this.gameObject.GetComponent<SpriteRenderer>().sprite==Viejo)
+        {
+            print("jala");
+            this.gameObject.GetComponent<SpriteRenderer>().sprite=Nuevo;
+            GManager.count = GManager.count + 1;
+             StartCoroutine(TirarRefresco());
+            //GameManager.instance.Win();
+        }
+
+        
+    }
+
+     void OnTriggerEnter2D(Collider2D collision) {
+        if(collision.gameObject.tag =="Player" && this.gameObject == door){
+        
+       StartCoroutine(TirarRefresco());
+        
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+      IEnumerator TirarRefresco()
+    {
+        yield return new WaitForSeconds(2);
+        SceneManager.LoadScene("DialogoTienda");
+    }
+}
