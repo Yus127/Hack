639d23f752e18fe38763ee69e653ec2dd199836d
Yusdivia Molina <yus.molina@gmail.com>
2021-05-22 20:45:19 -0500

    .

--- Assets/Scripts/navegacion/QuitGame.cs
+++ Assets/Scripts/navegacion/QuitGame.cs
@@ -8,7 +8,7 @@ public class QuitGame : MonoBehaviour
     // Start is called before the first frame update
     public void BtnNew()
     {
-        SceneManager.LoadScene("tienda");
+        SceneManager.LoadScene("fiesta");
     }
 
     // Update is called once per frame
---
+++ Assets/Scripts/navegacion/QuitGame2.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class QuitGame2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void BtnNew2()
+    {
+        SceneManager.LoadScene("AirHockey");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
