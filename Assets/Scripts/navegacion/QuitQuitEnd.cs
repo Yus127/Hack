6bc809ba9dc8b705400f90d7e30eb6897789fb72
Yusdivia Molina <yus.molina@gmail.com>
2021-05-22 21:42:19 -0500

    salirse bien de la app

---
+++ Assets/Scripts/navegacion/QuitQuitEnd.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitQuitEnd : MonoBehaviour
+{
+     public KeyCode key = KeyCode.Escape;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(key))
+        {
+            Application.Quit();
+        }
+    }
+/*
+    public void EXITtoMenu()
+    {
+        FindObjectOfType<GameManager>().Load(1);
+    }
+    */
+    public void EXIT()
+    {
+        Application.Quit();
+       
+    }
+}
